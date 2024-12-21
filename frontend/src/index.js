979009cb7cc10acb1b65ed276864f40873df3512
meow602 <riyajoy0509@gmail.com>
2024-12-21 17:05:04 +0530

    Add files via upload

---
+++ frontend/src/index.js
@@ -0,0 +1,16 @@
+// frontend/src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
