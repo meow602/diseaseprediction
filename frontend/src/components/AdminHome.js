c43e4a2a06f96457a53126552eb791c913227cbd
meow602 <riyajoy0509@gmail.com>
2024-12-21 21:27:59 +0530

    Create AdminHome.js

---
+++ frontend/src/components/AdminHome.js
@@ -0,0 +1,12 @@
+// frontend/src/components/AdminHome.js
+import React from 'react';
+
+function AdminHome() {
+  return (
+    <div>
+      <h2>Welcome to Admin Home Page</h2>
+    </div>
+  );
+}
+
+export default AdminHome;
